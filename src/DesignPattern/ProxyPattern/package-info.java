455c191b530817f195121c6a23048c4badc882b0
Zhiwei Lv <lzwheal@gmail.com>
2018-04-09 09:44:33 +0800

    add advantages and disadvantages

--- src/DesignPattern/ProxyPattern/package-info.java
+++ src/DesignPattern/ProxyPattern/package-info.java
@@ -1,9 +1,13 @@
 /**
  * @author Ethan
  * @desc 
- * ����ģʽ
- * ������ʵ����ϣ������������Ϊ�Ľӿ�Subject
- * ��ʵ����ʵ��Subject�ӿ�
- * ��������ʵ��Subject�ӿڣ������ڱ���������Ϊ����ʵ����ʵ����
+ * 代理模式
+ * 定义真实对象希望被代理的行为的接口Subject
+ * 真实对象实现Subject接口
+ * 代理对象实现Subject接口，并且在被代理的行为里面实现真实对象
+ * 优点：
+ * 代理对象作为客户端和目标对象间的中介，起到了保护目标对象的作用
+ * 缺点：
+ * 请求速度变慢；增加系统实现的复杂程度；
  */
 package DesignPattern.ProxyPattern;
